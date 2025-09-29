b0c66e182102e88121568d038b630de0c0187a38
satya2791 <boddupallisatyavalli2791@gmail.com>
2025-09-29 19:05:23 +0530

    Add files via upload
    Add a program that demonstrate about the different types of variables

---
+++ variable.js
@@ -0,0 +1,9 @@
+// Program to demonstrate variable declaration and initialization
+// Different ways to declare variables in JavaScript
+let Name = "Satya"; // let keyword to declare a variable which can be reassigned
+const PI = 18; // const keyword to declare a constant variable which cannot be reassigned
+var age = 18; // var keyword to declare a variable (function-swcoped, can be reassigned)
+// Print the variable values to the console
+console.log(Name);
+console.log(PI);
+console.log(age);
