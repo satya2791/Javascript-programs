363f22ecc285d303babbea9c272bf4e865c35cf2
satya2791 <boddupallisatyavalli2791@gmail.com>
2025-09-29 19:06:57 +0530

    Add a program that demonstrate about the different data types in the JavaScript

---
+++ datatypes.js
@@ -0,0 +1,16 @@
+//Program to demostrate different variable types 
+let Name = "Satya"; // string
+let age = 22; // number
+let isStudent = true; // boolean
+let marks; //undefind
+let address = null; // null
+let hobbies = ["reading", "travelling", "coding"]; // array
+let person = { name: "Satya", age: 18 }; // object
+// print the variable values and their types to the console
+console.log(typeof Name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof marks);
+console.log(typeof address);
+console.log(typeof hobbies);
+console.log(typeof person);
