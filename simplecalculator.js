4be8addc43496a022b1cab7071791114921d4c69
satya2791 <boddupallisatyavalli2791@gmail.com>
2025-09-30 17:25:36 +0530

    Add a program that performs basic arithmetic operations

---
+++ simplecalculator.js
@@ -0,0 +1,11 @@
+// Program to perform basic arithmetic operations
+// Initialize two numbers
+let a = 10;
+let b = 7;
+// Perform and display results of arithmetic operations
+console.log(`The addition of ${a} and ${b} is: ` + (a + b));
+console.log(`The subtraction of ${a} and ${b} is: ` + (a - b));
+console.log(`The multiplication of ${a} and ${b} is: ` + (a * b));
+console.log(`The division of ${a} and ${b} is: ` + (a / b));
+console.log(`The modulus of ${a} and ${b} is: ` + (a % b));
+console.log(`The exponentiation of ${a} and ${b} is: ` + (a ** b));
