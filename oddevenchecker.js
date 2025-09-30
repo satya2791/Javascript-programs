d7c3ede23d4bd228e70c0b9efc08521173d32993
satya2791 <boddupallisatyavalli2791@gmail.com>
2025-09-30 17:37:18 +0530

    Delete oddevenchecker.js

--- oddevenchecker.js
+++
@@ -1,8 +0,0 @@
-let n = 7;
-if (n == 0) {
-    console.log("The number is zero.");
-} else if (n % 2 == 0) {
-    console.log("The number is even.");
-} else {
-    console.log("The number is odd.");
-}
