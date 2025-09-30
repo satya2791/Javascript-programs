6f834bf559b006d2103918e07f15adfe892b26ec
satya2791 <boddupallisatyavalli2791@gmail.com>
2025-09-30 17:32:02 +0530

    Add a program that checks if a number is even or odd or zero.

---
+++ oddevenchecker.js
@@ -0,0 +1,8 @@
+let n = 7;
+if (n == 0) {
+    console.log("The number is zero.");
+} else if (n % 2 == 0) {
+    console.log("The number is even.");
+} else {
+    console.log("The number is odd.");
+}
