640fe0a165d289cfc9684e6f90e8455c8138d067
satya2791 <boddupallisatyavalli2791@gmail.com>
2025-10-01 17:09:30 +0530

    Add a program that prints the multiplication table for the given number

---
+++ multiplicationtable.js
@@ -0,0 +1,7 @@
+// Program to print the multiplication table of a given number
+// Initialize the number for which the multiplication table is to be printed
+let n = 9;
+// Loop through 1 to 10 and print the multiplication table
+for (let i = 1; i <= 10; i++) {
+    console.log(`${n} * ${i} = ${n * i}`);
+}
