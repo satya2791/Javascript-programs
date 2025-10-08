d481c5d0030b8fc2ad0a6b42f827e2a81ab2e64e
satya2791 <boddupallisatyavalli2791@gmail.com>
2025-10-08 21:30:44 +0530

    Add a program to find the sum of the elements of the array

---
+++ sum_of_array_elements.js
@@ -0,0 +1,12 @@
+// Program to find the sum of the elements in array
+// Initialize an array of numbers
+let arr = [25, 35, 45, 55, 65, 75, 85];
+// Initialize sum variable to 0
+let sum = 0;
+// Loop through the array to calculate the sum of elements
+for (let i = 0; i < arr.length; i++) {
+    // Store the sum of elements in sum variable
+    sum = sum + arr[i];
+}
+// Print the sum of the array elements
+console.log("The sum of the array elements is: " + sum);
