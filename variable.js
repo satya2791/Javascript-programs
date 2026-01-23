fca5a308f8f66171bda0bbe0bdcd4b3f94bef912
satya2791 <boddupallisatyavalli2791@gmail.com>
2026-01-23 22:00:00 +0530

    Add variables and console output example.

---
+++ variable.js
@@ -0,0 +1,8 @@
+// Declare a variable to store name
+let Name = "Satya";
+// Declare a variable to store age
+const age = 18;
+// Print name to the console
+console.log("Name: ",Name);
+// Print age to the console
+console.log("Age: ",age);
