4ae633e04fdb5884eab4fff4e2219532fa2f9d61
satya2791 <boddupallisatyavalli2791@gmail.com>
2026-01-25 18:23:43 +0530

    Add program JavaScript Type Conversion examples.

---
+++ typeConversion.js
@@ -0,0 +1,27 @@
+// String to Number and Boolean conversion
+let a = "25";
+
+// Converts string "25" to number 25
+let b = Number(a);
+
+// Any non-empty string converts to true
+let c = Boolean(a);
+
+console.log(b); // 25
+console.log(c); // true
+console.log(`type of ${b} is`, typeof b);
+console.log(`type of ${c} is`, typeof c);
+
+// Number to String and Boolean conversion
+let d = 45;
+
+// Converts number 45 to string "45"
+let e = String(d);
+
+// Any non-empty string converts to true
+let f = Boolean(e);
+
+console.log(e); // "45"
+console.log(f); // true
+console.log(`type of ${e} is`, typeof e);
+console.log(`type of ${f} is`, typeof f);
