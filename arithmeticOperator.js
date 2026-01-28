4ff78b1e7df384ef94b4c35dc4a7adfe67b33c09
satya2791 <boddupallisatyavalli2791@gmail.com>
2026-01-28 23:27:12 +0530

    Add basic calculator logic.

---
+++ arithmeticOperator.js
@@ -0,0 +1,8 @@
+let a = Number(prompt("Enter the first number: "));
+let b = Number(prompt("Enter the second number: "));
+
+console.log(`The Addition of ${a} and ${b} is: ${a + b}`);
+console.log(`The Subtraction of ${a} and ${b} is: ${a - b}`);
+console.log(`The Multiplication of ${a} and ${b} is: ${a * b}`);
+console.log(`The Division of ${a} and ${b} is: ${a / b}`);
+console.log(`The Modulus of ${a} and ${b} is: ${a % b}`);
