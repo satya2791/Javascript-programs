0b22f4fa4127eeb4781460794f0be15d35e064cf
satya2791 <boddupallisatyavalli2791@gmail.com>
2026-01-29 21:29:06 +0530

    Delete logicalOperator.js

--- logicalOperator.js
+++
@@ -1,7 +0,0 @@
-let a = Number(prompt("Enter the first number: "));
-let b = Number(prompt("Enter the second number: "));
-
-console.log(`a > 0 AND b > 0 : ${a > 0 && b > 0}`);
-console.log(`a > 0 OR b > 0  : ${a > 0 || b > 0}`);
-console.log(`NOT (a > 0)     : ${!(a > 0)}`);
-console.log(`NOT (b > 0)     : ${!(b > 0)}`);
