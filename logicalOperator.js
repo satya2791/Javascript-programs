3e04814d9b2a556f2857debba9ecac9018b1a0ef
satya2791 <boddupallisatyavalli2791@gmail.com>
2026-01-29 21:30:53 +0530

    Add examples of logical operator.

---
+++ logicalOperator.js
@@ -0,0 +1,21 @@
+// Take first number input from user and convert it to Number
+let a = Number(prompt("Enter the first number: "));
+
+// Take second number input from user and convert it to Number
+let b = Number(prompt("Enter the second number: "));
+
+// Logical AND (&&)
+// Returns true only if BOTH conditions are true
+console.log(`a > 0 AND b > 0 : ${a > 0 && b > 0}`);
+
+// Logical OR (||)
+// Returns true if AT LEAST ONE condition is true
+console.log(`a > 0 OR b > 0  : ${a > 0 || b > 0}`);
+
+// Logical NOT (!)
+// Reverses the boolean result of the condition
+console.log(`NOT (a > 0)     : ${!(a > 0)}`);
+
+// Logical NOT (!)
+// Reverses the boolean result of the condition
+console.log(`NOT (b > 0)     : ${!(b > 0)}`);
